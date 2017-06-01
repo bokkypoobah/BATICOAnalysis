4f74ca069123772bd36e9d7df7740bd008a53dda
BokkyPooBah <bokky.poobah@bokconsulting.com.au>
2017-06-01 17:38:50 +1000

    Added gasUsed, gasPrice and gas cost as suggested by Joe Richards

--- scripts/getBATICOData.sh
+++ scripts/getBATICOData.sh
@@ -17,7 +17,7 @@ var icoLastTxBlock = parseInt(icoFirstTxBlock) + 500;
 var totalEthers = new BigNumber(0);
 var totalTokens = new BigNumber(0);
 
-console.log("RESULT: Account\tBlock\tTxIdx\t#\tEthers\tSumEthers\tTokens\tSumTokens\tTimestamp\tDateTime\tTxHash");
+console.log("RESULT: Account\tBlock\tTxIdx\t#\tEthers\tSumEthers\tTokens\tSumTokens\tTimestamp\tDateTime\tTxHash\tGasUsed\tGasPrice\tGasCost");
 
 function getEtherData() {
   var count = 0;
@@ -42,8 +42,13 @@ function getEtherData() {
           var tokenBalancePrev = token.balanceOf(e.from, parseInt(e.blockNumber) - 1).div(1e18);
           var tokenBalance = token.balanceOf(e.from, e.blockNumber).div(1e18).minus(tokenBalancePrev);
           totalTokens = totalTokens.add(tokenBalance);
+          var txr = eth.getTransactionReceipt(e.hash);
+          var gasUsed = new BigNumber(txr.gasUsed);
+          var gasPrice = e.gasPrice;
+          var gasCost = gasUsed.times(gasPrice);
           console.log("RESULT: " + e.from + "\t" + e.blockNumber + "\t" + e.transactionIndex + "\t" + count + "\t" + ethers + "\t" + 
-            totalEthers + "\t" + tokenBalance + "\t" + totalTokens + "\t" + timestamp + "\t" + time.toUTCString() + "\t" + e.hash);
+            totalEthers + "\t" + tokenBalance + "\t" + totalTokens + "\t" + timestamp + "\t" + time.toUTCString() + "\t" + e.hash +
+            "\t" + gasUsed + "\t" + web3.fromWei(gasPrice, "ether") + "\t" + web3.fromWei(gasCost, "ether"));
         }
       });
     }
